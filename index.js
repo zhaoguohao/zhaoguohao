f36796e2174790581a8bec1cbbbd53230b3f4a43
zhaoguohao <43693203+zhaoguohao@users.noreply.github.com>
2023-01-13 03:22:37 +0800

    Update index.js

--- index.js
+++ index.js
@@ -22,14 +22,7 @@ const readme = `\
 
 Here are some ideas to get you started:
 
-- 🔭 0
-- 🌱 1
-- 👯 0
-- 🤔 1
-- 💬 0
-- 📫 1
-- ⚡ Fun fact: biu~
-- 😄 Pronouns: 哇这是帝企鹅.
+- 😄 Fun fact: biu~ 哇这是帝企鹅.
 
 [![Anurag's github stats](https://github-readme-stats-git-masterrstaa-rickstaa.vercel.app/api?username=zhaoguohao&show_icons=true&count_private=true&custom_title=Hao's%20GitHub%20profile&theme=vision-friendly-dark&line_height=40&hide_border=true)](https://github.com/anuraghazra/github-readme-stats)
 [![Anurag's github stats](https://github-readme-stats-git-masterrstaa-rickstaa.vercel.app/api/top-langs/?username=zhaoguohao&custom_title=Hao's%20code%20language&theme=radical&line_height=10hide_border=true&hide=html&exclude_repo=python_vim)](https://github.com/anuraghazra/github-readme-stats)
